eebb655a937df94e9b1e5d1a3b0b6738c524c37b
Gabriel dos Santos Sanches <gabrielssanches@gmail.com>
2023-10-30 10:50:24 +0100

    wip

--- slock_raylib.c
+++ slock_raylib.c
@@ -31,7 +31,8 @@ static char _passwd[32];
 #define CELL_ALIVE  (1 << 0)
 #define CELL_BORN   (1 << 1)
 #define CELL_KILL   (1 << 2)
-#define CELL_GET_AGE(x)  (((x) & 0xFFFF0000) >> 4)
+#define CELL_GET_AGE(x)  (((x) & 0xFFFF0000) >> 16)
+#define CELL_INC_AGE(x)  ((x) + 0x10000)
 
 static void gol_create(struct gol* gol, const int ncells_horizontal, const int ncells_vertical) {
     gol->cell_nv = ncells_vertical;
@@ -78,6 +79,16 @@ static bool gol_cell_is_alive(struct gol* gol, const int col, const int line) {
     return alive;
 }
 
+static int gol_cell_age(struct gol* gol, const int col, const int line) {
+    int i = gol_cell_index(gol, col, line);
+    return CELL_GET_AGE(gol->cell_array[i]);
+}
+
+static void gol_cell_age_inc(struct gol* gol, const int col, const int line) {
+    int i = gol_cell_index(gol, col, line);
+    gol->cell_array[i] += 0x10000;
+}
+
 static void gol_cell_mark_kill(struct gol* gol, const int col, const int line) {
     int i = gol_cell_index(gol, col, line);
     gol->cell_array[i] |= CELL_KILL;
@@ -108,9 +119,25 @@ static void gol_solve(struct gol* gol) {
             // 2. Any live cell with two or three live neighbours lives on to the next generation.
             // 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
             // 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+            // 5. Any live cell with that is alive for more than 100 cycles explodes and dies, giving its live to all other its neighbours.
             if (gol_cell_is_alive(gol, col, line)) {
+                gol_cell_age_inc(gol, col, line);
                 if (n_alive < 2 || n_alive > 3) {
                     gol_cell_mark_kill(gol, col, line);
+                } else {
+                    int age = gol_cell_age(gol, col, line);
+                    if (age > 100) {
+                        gol_cell_mark_kill(gol, col, line);
+
+                        gol_cell_mark_born(gol, col -1, line -1);
+                        gol_cell_mark_born(gol, col, line -1);
+                        gol_cell_mark_born(gol, col +1, line -1);
+                        gol_cell_mark_born(gol, col -1, line);
+                        gol_cell_mark_born(gol, col +1, line);
+                        gol_cell_mark_born(gol, col -1, line +1);
+                        gol_cell_mark_born(gol, col, line +1);
+                        gol_cell_mark_born(gol, col +1, line +1);
+                    }
                 }
             } else {
                 if (n_alive == 3) {
@@ -121,7 +148,7 @@ static void gol_solve(struct gol* gol) {
     }
     for (int i = 0; i < (gol->cell_nh * gol->cell_nv); i++) {
         if ((gol->cell_array[i] & CELL_KILL) != 0) {
-            gol->cell_array[i] &= ~(CELL_KILL | CELL_ALIVE);
+            gol->cell_array[i] = 0;
         }
         if ((gol->cell_array[i] & CELL_BORN) != 0) {
             gol->cell_array[i] &= ~CELL_BORN;
@@ -168,7 +195,7 @@ void slock_raylib_init(void) {
 #endif
     memset(&_passwd, 0, sizeof(_passwd));
 
-    _g.grid.size = 10;
+    _g.grid.size = 5;
     _g.grid.nh = _g.display.width / _g.grid.size;
     _g.grid.nv = _g.display.height / _g.grid.size;
     gol_create(&_g.gol, _g.grid.nh, _g.grid.nv);
